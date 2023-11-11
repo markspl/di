9f592ef6f823432583eeafdfe0e8288d81969779
Markus Palomäki <markus.palomaki@futurice.com>
2023-11-11 18:20:58 +0200

    create main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Header from './components/Header/Header';
+import Main from './screens/main/Main';
 import './App.css';
 
 function App() {
     return (
         <div className="app">
-            <Header />
+            <Main />
         </div>
     );
 }
---
+++ src/screens/main/Main.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header';
+import './Main.css';
+
+function Main() {
+    return (
+        <main>
+            <Header />
+        </main>
+    );
+}
+
+export default Main;
