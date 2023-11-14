c2693dadc04ead7f91a3655f522c10ede691dc10
Markus Palomäki <markus.palomaki@futurice.com>
2023-11-14 19:47:43 +0200

    add empty footer element

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import './Footer.scss';
+
+function Footer() {
+    return <footer></footer>;
+}
+
+export default Footer;
--- src/screens/main/Main.tsx
+++ src/screens/main/Main.tsx
@@ -1,5 +1,6 @@
 import Cards from '../../components/Cards/Cards';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import './Main.css';
 
 function Main() {
@@ -7,6 +8,7 @@ function Main() {
         <main>
             <Header />
             <Cards />
+            <Footer />
         </main>
     );
 }
